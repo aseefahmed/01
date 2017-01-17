65019209a329a1b0a30c02c017f94b77a69a8ecd
Aseef Ahmed <aseefahmed@gmail.com>
2017-01-17 17:56:39 +0600

    44

--- modules/Buying/Http/routes.php
+++ modules/Buying/Http/routes.php
@@ -2,8 +2,11 @@
 
 Route::group(['middleware' => 'web', 'prefix' => 'buying', 'namespace' => 'Modules\Buying\Http\Controllers'], function()
 {
-	Route::get('/', 'BuyingController@index');
-	Route::post('orders/store', ['uses' => 'BuyingController@storeOrders', 'as' => 'Orders']);
-	Route::get('/order/fetchOrdersList/{user_id}/{emp_role}', ['uses' => 'BuyingController@fetchOrdersList', 'as' => 'Orders List']);
+    Route::get('/', 'BuyingController@index');
+    Route::post('orders/store', ['uses' => 'BuyingController@storeOrders', 'as' => 'Orders']);
+    Route::get('/order/fetchOrdersList/{user_id}/{emp_role}', ['uses' => 'BuyingController@fetchOrdersList', 'as' => 'Orders List']);
+    Route::get('/order/fetchOrdersList/{user_id}/{emp_role}', ['uses' => 'BuyingController@fetchOrdersList', 'as' => 'Orders List']);
+    Route::get('/order/fetchOrderDetails/{id}', ['uses' => 'BuyingController@fetchOrderDetails', 'as' => 'Order Details']);
+    Route::get('/order/update/{user_id}/{field}/{id}/{value}', ['uses' => 'BuyingController@updateField', 'as' => 'Update Information']);
 	
 });
